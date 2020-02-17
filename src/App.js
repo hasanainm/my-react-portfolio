759f5f937f386ddf7a008681e89ec0364b0e5224
Hasanain Mohammed <hasanain98@gmail.com>
2020-02-17 02:01:38 -0600

    profile complete

--- src/App.js
+++ src/App.js
@@ -17,24 +17,23 @@ class App extends Component {
       <Skills/>
       <CliGrid />
       <Projects/>
-      <section className="webApps-list u-padding-bottom-tiny u-padding-top-tiny u-margin-top-tiny">
+      <section className="webApps-list u-padding-bottom-small u-padding-top-tiny u-margin-top-tiny">
       <h1 className="heading-primary--webApps">Other Web-Apps</h1>
       <div class="row">
       <div class="col-1-of-4">
-      <OtherWebApps iconClass ="fas fa-dollar-sign icons" github ="https://github.com/hasanainm/GoHam-portfolio-builder" heroku ="https://goham-portfolio-builder.herokuapp.com/" name={"Budget App"} glink={"View Code"} hlink={"View App"} description={"cool app"}></OtherWebApps>
+      <OtherWebApps iconClass ="fas fa-dollar-sign icons" github ="https://github.com/hasanainm/budgetApp" heroku ="https://hasanainm.github.io/budgetApp/" name={"Budget App"} glink={"View Code"} hlink={"View App"}></OtherWebApps>
       </div>
       <div class="col-1-of-4">
-      <OtherWebApps iconClass ="fas fa-dollar-sign icons" github ="https://github.com/hasanainm/GoHam-portfolio-builder" heroku ="https://goham-portfolio-builder.herokuapp.com/" name={"Budget App"} glink={"View Code"} hlink={"View App"} description={"cool app"}></OtherWebApps>
+      <OtherWebApps iconClass ="fas fa-hand-pointer icons" github ="https://github.com/hasanainm/clicky-game" heroku ="https://marvel-clicky.herokuapp.com/" name={"Clicky Game"} glink={"View Code"} hlink={"View App"}></OtherWebApps>
       </div>
       <div class="col-1-of-4">
-      <OtherWebApps iconClass ="fas fa-dollar-sign icons" github ="https://github.com/hasanainm/GoHam-portfolio-builder" heroku ="https://goham-portfolio-builder.herokuapp.com/" name={"Budget App"} glink={"View Code"} hlink={"View App"} description={"cool app"}></OtherWebApps>
+      <OtherWebApps iconClass ="fas fa-train icons" github ="https://github.com/hasanainm/Train-Scheduler" heroku ="https://hasanainm.github.io/Train-Scheduler/" name={"Train Scheduler"} glink={"View Code"} hlink={"View App"}></OtherWebApps>
       </div>
       <div class="col-1-of-4">
-      <OtherWebApps iconClass ="fas fa-dollar-sign icons" github ="https://github.com/hasanainm/GoHam-portfolio-builder" heroku ="https://goham-portfolio-builder.herokuapp.com/" name={"Budget App"} glink={"View Code"} hlink={"View App"} description={"cool app"}></OtherWebApps>
+      <OtherWebApps iconClass ="fas fa-image icons" github ="https://github.com/hasanainm/GifTastic" heroku ="https://hasanainm.github.io/GifTastic/" name={"Giphy Api"} glink={"View Code"} hlink={"View App"}></OtherWebApps>
       </div>
     </div>
       </section>
-    
       <Footer />
       </div>
     );
--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -8,7 +8,7 @@ const AboutMe = () => {
       <div className="col-1-of-2 u-left-text bio u-margin-top-small">
         <h1 className="heading-primary--other u-margin-bottom-tiny">Born and raised in Houston, Texas</h1>
         <hr className="line"/>
-        <p className="u-margin-top-tiny heading-primary--bio">My name is Hasanain Mohammed and I am a Full-Stack Web Developer. I'm also a UH Cougar, Houstonian, and a tech aficionado. As a freelancer, I love learning new tecnologies and creating projects. As I grow as a developer I want to travel, meet new people, and talk about how things code!</p>
+        <p className="u-margin-top-tiny heading-primary--bio">My name is Hasanain Mohammed and I am a Full-Stack Web Developer. I'm also a UH Cougar, Houstonian, and a tech aficionado. As a freelancer, I love learning new technologies and creating projects. As I grow as a developer I want to travel, meet new people, and talk about how things code!</p>
       </div>
       <div className="col-1-of-2">
         <img src="/img/hasanain.png" className="mesize" alt="me"/>
